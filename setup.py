88e8967a5769efc2b9749b9595cf23e625b0682e
xinshi-chen <xinshi.chen@gatech.edu>
2021-03-15 13:29:41 -0400

    pytorch version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ BASEPATH = os.path.dirname(os.path.abspath(__file__))
 setup(name='vae4dag',
       py_modules=['vae4dag'],
       install_requires=[
-          'torch==v1.4.0',
+          'torch==v1.8.0',
           'numpy',
           'tqdm',
           'networkx',
