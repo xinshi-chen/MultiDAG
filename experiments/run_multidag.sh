0e439633f546d1387aa6fa636a11accabb763e80
riemannnn@gmail.com <riemannnn@gmail.com>
2021-05-07 13:30:38 -0700

    update configs

--- experiments/run_multidag.sh
+++ experiments/run_multidag.sh
@@ -3,20 +3,22 @@
 p=(32 128 512)
 K=64
 n=(20 40 80 160 320 640)
-s=(12 18 24)
-s0=(8 12 16)
-d=4
+s=(160 1000 6000)
+s0=(64 384 2048)
+d=(4 6 8)
+gpu=(5 6 1 4 0 7)
 num_epoches=4000
 
 for i in ${!p[*]}; do
-  for j in ${!n[*]}; do
+  for j in 5; do
     python main_linear.py \
     -p ${p[i]} \
     -K ${K} \
     -n ${n[j]} \
     -s ${s[i]} \
     -s0 ${s0[i]} \
-    -d ${d} \
-    -num_epoches ${num_epoches}
+    -d ${d[i]} \
+    -num_epochs ${num_epoches} \
+    -gpu ${gpu[j]}
   done
 done
