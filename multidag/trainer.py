4334e9877b6b2dd0f8caaeab3b027bdee6e6b3aa
riemannnn@gmail.com <riemannnn@gmail.com>
2021-05-12 13:09:03 -0700

    update hyperpara

--- multidag/trainer.py
+++ multidag/trainer.py
@@ -116,19 +116,20 @@ class Trainer:
         mu_one = self.hyperparameter['mu'] * one
 
         conn = h_W[self.constraint_type](self.g_dag.T)
-        lambda_conn = ld.mean() * conn
-        c_conn_2 = 0.5 * c.mean() * conn ** 2
+        lambda_conn = ld.mean() * np.sqrt(X.shape[0]) * conn
+        c_conn_2 = 0.5 * np.sqrt(X.shape[0]) * c.mean() * conn ** 2
 
         h_D = h_W[self.constraint_type](G_D)
         # if h_D.sum().item() == 0:
         #     for i in range(len(h_D)):
         #         assert is_dag(G_D[i].detach().numpy())
-        lambda_h_wD = (ld * h_D).mean()  # lagrangian term
-        c_hw_2 = 0.5 * (c * h_D * h_D).mean()  # l2 penalty
+        lambda_h_wD = (ld * h_D).mean() * np.sqrt(X.shape[0])  # lagrangian term
+        c_hw_2 = 0.5 * np.sqrt(X.shape[0]) * (c * h_D * h_D).mean()  # l2 penalty
 
         # group norm
         w_l1_l2 = torch.linalg.norm(G_D, ord=2, dim=0).sum()
-        rho_w_l1 = self.hyperparameter['rho'] * np.sqrt((self.db.K) / (X.shape[0])) * w_l1_l2
+        rho_w_l1 = self.hyperparameter['rho'] * np.sqrt((self.db.K) / (X.shape[0])) * \
+                   np.sqrt(self.db.p * np.log(32) / (32 * np.log(self.db.p))) * w_l1_l2
 
         loss = loss_se + lambda_conn + c_conn_2 + mu_one + rho_w_l1 + lambda_h_wD + c_hw_2
 
