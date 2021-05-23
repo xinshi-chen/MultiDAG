4737910f910ba037e700a5eeecae9b8ce7f70b3b
riemannnn@gmail.com <riemannnn@gmail.com>
2021-05-23 10:21:51 -0700

    update configs for hive

--- multidag/trainer.py
+++ multidag/trainer.py
@@ -138,7 +138,7 @@ class Trainer:
 
         # group norm
         w_l1_l2 = torch.linalg.norm(G_D, ord=2, dim=0).sum()
-        rho_w_l1 = self.rho * w_l1_l2 / X.shape[0] / np.sqrt(self.db.n / 10)
+        rho_w_l1 = self.rho * w_l1_l2 / X.shape[0] / (self.db.n / 10)
 
         loss = loss_se + lambda_conn + c_conn_2 + mu_one + rho_w_l1 + lambda_h_wD + c_hw_2
 
