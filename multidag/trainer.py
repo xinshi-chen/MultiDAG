c230ecabf5ddcf07cf8c9df565ace293b51a82e7
riemannnn@gmail.com <riemannnn@gmail.com>
2021-05-23 20:01:45 -0700

    update config

--- multidag/trainer.py
+++ multidag/trainer.py
@@ -103,9 +103,9 @@ class Trainer:
             self.gamma *= 0.99
             for param_group in self.optimizer.param_groups:
                 param_group['lr'] *= 0.99
-            if log['SE'] / self.se > log['l1/l2'] / self.gn + 0.05:
+            if log['SE'] / self.se > 1.05:
                 self.rho *= 0.95
-            elif log['SE'] / self.se < log['l1/l2'] / self.gn - 0.1:
+            elif log['SE'] / self.se < 0.95:
                 self.rho *= 1.05
             self.ld = torch.clamp(self.ld + self.c * (1e3 - (1e3 - h_D) * ((1e3 - h_D) > 0)), min=0, max=1e12)
             self.c = torch.clamp(self.c * (1 + self.hyperparameter['eta']), min=0, max=1e15)
